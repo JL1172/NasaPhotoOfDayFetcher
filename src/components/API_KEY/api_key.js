0b20102718c25db3a6bbcde04a5dbf25ece4a397
Jacob Lang <jacoblang72@comcast.net>
2023-09-05 10:30:08 -0400

    firstfirst

--- src/components/API_KEY/api_key.js
+++ src/components/API_KEY/api_key.js
@@ -2,6 +2,6 @@ import { useEffect, useState } from "react";
 import React from "react";
 import axios from "axios";
 
-export const API_KEY = 'kDNGcwxyNNZQaaCcbUbdhGjP1jVB68YbmpkhjlS4';
+export const API_KEY = "";
 export const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 
